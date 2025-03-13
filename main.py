b385295ea7d541f3d83cc9e2c8b028555241f15a
bman-prog <bmankovski2028@communityschoolnaples.org>
2025-03-13 12:00:57 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,31 @@
+hero = sprites.create(img("""
+        . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . . . . b c . . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . . . . b b 5 d 1 f 5 5 d f . . 
+            . . . . b 5 5 1 f f 5 d 4 c . . 
+            . . . . b 5 5 d f b d d 4 4 . . 
+            b d d d b b d 5 5 5 4 4 4 4 4 b 
+            b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+            b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+            c d d c d 5 5 b 5 5 5 5 5 5 b . 
+            c b d d c c b 5 5 5 5 5 5 5 b . 
+            . c d d d d d d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . .
+    """),
+    SpriteKind.player)
+controller.move_sprite(hero)
+
+number = randint(1, 10)
+guess = game.ask_for_number("Choose a random number")
+
+
+if guess > 10:
+    hero.say_text("The number is too large")
+
+if guess < 1:
+    hero.say_text("The number is too small")
+ 
